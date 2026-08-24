b94f0337fddc05d709ed69b0f8d3eb0fab67a151
Riina <riina.e.aaltonen@gmail.com>
2026-08-24 09:26:38 +0200

    first commit

---
+++ install.js
@@ -0,0 +1,56 @@
+const { Client } = require("pg");
+require("dotenv").config(); 
+
+const client = new Client({
+    host: process.env.DB_HOST, 
+    port: process.env.DB_PORT,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE, 
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
+
+client.connect((err ) => {
+     if (err) {
+        console.log("Connection error: " + err);
+    } else {
+        console.log("Connected to database!");
+        createTables(); 
+    };
+}); 
+
+async function createTables() {
+
+    try {
+        const res = await client.query(`
+            DROP TABLE IF EXISTS courses;
+            DROP TABLE IF EXISTS profile; 
+            CREATE TABLE IF NOT EXISTS profile (
+                profile_id SERIAL PRIMARY KEY,
+                firstname TEXT NOT NULL,
+                lastname TEXT NOT NULL,
+                occupation TEXT NOT NULL,
+                email TEXT NOT NULL UNIQUE, 
+                telephone TEXT NOT NULL
+                 );
+            CREATE TABLE IF NOT EXISTS courses (
+                course_id SERIAL PRIMARY KEY,
+                profile_id INTEGER NOT NULL, 
+                coursename TEXT NOT NULL, 
+                coursecode TEXT NOT NULL, 
+                syllabus TEXT NOT NULL, 
+                progression TEXT NOT NULL, 
+                
+                FOREIGN KEY (profile_id)
+                    REFERENCES profile(profile_id)
+                    ON DELETE CASCADE
+                );
+        `)
+    } catch (err) {
+        console.log(err)
+    } finally {
+        await client.end()
+    }
+}
---
+++ server.js
@@ -0,0 +1,168 @@
+const { Client } = require("pg");
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs")
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }))
+
+const client = new Client({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
+
+client.connect((err) => {
+    if (err) {
+        console.log("Connection error: " + err);
+    } else {
+        console.log("Connected to database!");
+    };
+});
+
+//Routing
+app.get("/", async (req, res) => {
+
+    try {
+        const profile = await client.query(
+            "SELECT * FROM profile;"
+        )
+        const courses = await client.query(
+            "SELECT * FROM courses WHERE profile_id = 1;"
+        )
+        res.render("index", { 
+            profile: profile.rows[0],
+            courses: courses.rows
+        })
+    } catch (error) {
+        console.log(error)
+    }
+});
+
+app.get("/create", (req, res) => {
+    res.render("create", {
+        profileErrors: {},
+        courseErrors: {}
+    })
+})
+
+app.get("/about", (req, res) => {
+    res.render("about");
+});
+
+app.post("/save-profile", async (req, res) => {
+    const fname = req.body.fname;
+    const lname = req.body.lname;
+    const occupation = req.body.occupation;
+    const email = req.body.email;
+    const telephone = req.body.telephone;
+    let profileErrors = {}; 
+
+    if (fname === "") {
+        profileErrors.fname = "Förnamn måste fyllas i och får inte vara tomt."
+    }
+
+    if (lname === "") {
+        profileErrors.lname = "Efternamn måste fyllas i och får inte vara tomt."
+    }
+
+    if (occupation === "") {
+        profileErrors.occupation = "Du måste välja arbetstitel."
+    }
+
+    if (email === "" || email.includes("@") === false) {
+        profileErrors.email = "E-postadressen behöver innehålla ett @."
+    }
+
+    if (telephone === "") {
+        profileErrors.telephone = "Telefonnummer måste fyllas i och får inte vara tomt."
+    }
+  
+    if (Object.keys(profileErrors).length > 0) {
+        res.render("create", {
+            profileErrors: profileErrors,
+            courseErrors: {}
+        });
+        return;
+    }
+       
+    try {
+        const result = await client.query(
+            "INSERT INTO profile(firstname, lastname, occupation, email, telephone) VALUES($1, $2, $3, $4, $5)", [fname, lname, occupation, email, telephone]
+        );
+        res.redirect("create");
+        return;
+
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+app.post("/save-course", async (req, res) => {
+    const coursename = req.body.coursename;
+    const coursecode = req.body.coursecode;
+    const syllabus = req.body.syllabus;
+    const progression = req.body.progression;
+    let courseErrors = {}; 
+
+    if (coursename === "") {
+        courseErrors.coursename = "Kursnamn måste fyllas i och får inte vara tomt."
+    }
+
+    if (coursecode === "") {
+        courseErrors.coursecode = "Kurskod måste fyllas i och får inte vara tomt."
+    }
+
+    if (syllabus === "") {
+        courseErrors.syllabus = "Syllabus måste fyllas i och får inte vara tomt."
+    }
+
+    if (progression === "") {
+        courseErrors.progression = "Du måste välja ett progressionsalternativ."
+    }
+  
+    if (Object.keys(courseErrors).length > 0) {
+        res.render("create", {
+            courseErrors: courseErrors,
+            profileErrors: {}
+        });
+        return;
+    }
+
+    try {
+        const getProfile = await client.query("SELECT profile_id FROM profile;")
+        const profile_id = getProfile.rows[0].profile_id;
+        const result = await client.query(
+            "INSERT INTO courses(profile_id, coursename, coursecode, syllabus, progression) VALUES($1, $2, $3, $4, $5)", [profile_id, coursename, coursecode, syllabus, progression]
+        );
+        res.redirect("create")
+        return;
+
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+app.post("/delete-course", async (req, res) => {
+    const courseID = req.body.course_id; 
+
+    await client.query(
+        "DELETE FROM courses WHERE course_id = $1", [courseID]
+    ); 
+
+    res.redirect("/");
+})
+
+//Starta applikation
+app.listen(process.env.PORT, () => {
+    console.log("Server started at http://localhost:" + process.env.PORT);
+});
+
